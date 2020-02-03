d8bd0f0df67e896a47b4b7a83bb8062315e2e5b0
KyotaHoshi <kyota-h.8@outlook.jp>
2020-02-03 23:55:07 +0900

    課題提出

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { takeshi, mikio } from "./lib.js"
+
+mikio("コード書いてるとき")
+
+console.log(takeshi);
+
+import "./style.scss"
---
+++ src/lib.js
@@ -0,0 +1,8 @@
+const takeshi = "間違いないね。"
+
+const mikio = (thing) => {
+    console.log(`世の中興奮することいっぱいあるけど、一番興奮するのは${thing}だね。`);
+    
+}
+
+export { takeshi, mikio }
